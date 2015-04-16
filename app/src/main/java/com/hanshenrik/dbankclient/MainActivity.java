d31edc36bacb393572b36e830606927e9166267b
hanshenrik <hh.gronsleth@gmail.com>
2015-04-16 16:24:55 +0100

    handle SettingsActivity, created variables for the buttons and input fields

--- app/src/main/java/com/hanshenrik/dbankclient/MainActivity.java
+++ app/src/main/java/com/hanshenrik/dbankclient/MainActivity.java
@@ -1,17 +1,82 @@
 package com.hanshenrik.dbankclient;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int SETTINGS_REQUEST = 1;
+
+    private Button getBalanceButton;
+    private Button withdrawButton;
+    private Button depositButton;
+    private TextView balanceText;
+    private EditText amountInput;
+    private ListView accountNumbersListView;
+
+    private ArrayList<String> accountNumbers;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d("1", "");
+        if (resultCode == RESULT_CANCELED) { // user clicked back button in SettingsActivity
+            Log.d("2", "");
+            if (requestCode == SETTINGS_REQUEST) {
+                Log.d("3", "");
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+                Map<String, ?> prefsMap = prefs.getAll();
+                for (String key : prefsMap.keySet()) {
+                    Log.d("MAP", key + " | " + prefsMap.get(key).toString());
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        accountNumbers = new ArrayList<>();
+        // DEV
+        accountNumbers.add("1234.56.78910");
+        accountNumbers.add("9876.54.32100");
+        accountNumbers.add("0000.00.00000");
+
+        getBalanceButton = (Button) findViewById(R.id.getBalanceButton);
+        withdrawButton = (Button) findViewById(R.id.withdrawButton);
+        depositButton = (Button) findViewById(R.id.depositButton);
+        balanceText = (TextView) findViewById(R.id.balanceTextView);
+        amountInput = (EditText) findViewById(R.id.amountInput);
+        accountNumbersListView = (ListView) findViewById(R.id.accountNumbersListView);
+
+        ArrayAdapter accountNumbersListAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, accountNumbers);
+
+        getBalanceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                balanceText.setText("getting balance...");
+                // query balance
+                // on some callback
+                balanceText.setText(getString(R.string.currency_prefix) + "100.00");
+            }
+        });
     }
 
 
@@ -31,6 +96,8 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+            startActivityForResult(intent, SETTINGS_REQUEST);
             return true;
         }
 
